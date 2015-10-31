f6080fa6f03567ce945a9bb13952c47cade216bb
Duilio <silence@dessigual.com>
2015-10-31 11:44:31 +0000

    Dynamic dropdowns with AJAX

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Cars\Models\MakeYear;
+use Cars\Models\Model;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,4 +24,28 @@ Route::get('bootstrap', function () {
 
 Route::get('dropdowns', function () {
     return view('components/dropdowns');
+});
+
+Route::get('makeyears/{make_id}', function ($make_id) {
+    $years = MakeYear::where('make_id', $make_id)
+        ->select('id as value', 'year as text')
+        ->orderBy('year', 'DESC')
+        ->get()
+        ->toArray();
+
+    array_unshift($years, ['value' => '', 'text' => 'Select value']);
+
+    return $years;
+});
+
+Route::get('models/{makeyear_id}', function ($makeyear_id) {
+    $models = Model::where('makeyear_id', $makeyear_id)
+        ->select('id as value', 'name as text')
+        ->orderBy('name', 'ASC')
+        ->get()
+        ->toArray();
+
+    array_unshift($models, ['value' => '', 'text' => 'Select value']);
+
+    return $models;
 });
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -61,10 +61,48 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
-        $('select').select2();
-        $('#search select').change(function () {
-           $('#search').submit();
+        $('select').select2({
+            allowClear: true,
+            placeholder: {
+                id: "",
+                text: 'Select value'
+            }
         });
+
+        $.fn.populateSelect = function (values) {
+            var options = '';
+            $.each(values, function (key, row) {
+                options += '<option value="' + row.value + '">' + row.text + '</option>';
+            });
+            $(this).html(options);
+        }
+
+        $('#make_id').change(function () {
+            $('#model_id').empty().change();
+
+            var make_id = $(this).val();
+
+            if (make_id == '') {
+                $('#makeyear_id').empty().change();
+            } else {
+                $.getJSON('/makeyears/'+make_id, null, function (values) {
+                    $('#makeyear_id').populateSelect(values);
+                });
+            }
+        });
+
+        $('#makeyear_id').change(function () {
+           var year = $(this).val();
+
+            if (year == '') {
+                $('#model_id').empty().change();
+            } else {
+                $.getJSON('/models/'+year, null, function (values) {
+                    $('#model_id').populateSelect(values);
+                });
+            }
+        });
+
     });
 </script>
 </body>
